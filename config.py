0e5f75c9ccfce39228694f04542da35507cd348e
Leo <450019458@qq.com>
2025-03-26 15:35:02 +0800

    feat(models): 添加 DeepSeek V3 官方大模型支持
    - 新增 DeepSeekV3 类以支持 DeepSeek V3 官方大模型
    - 在 config.py 中添加 DeepSeekV3 模型的引用和标识
    - 实现了与 DeepSeek API 的交互，包括流式响应处理

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ from class_interface import ClassInterface
 from models.ark.dipuDada import DipuDada
 from models.ark.deepseekModel import DeepSeekArk
 from models.ark.doubao256kModel import DouBao256kModel
+from models.deepseek.deepseek_code import DeepSeekV3
 from models.google.gemini import Gemini
 from models.moonShort.moonshot import Moonshot
 from models.mistral.mistralLarge import Mistral
 from models.openAI.chatGPT import OpenAI
 from models.siliconFlow.siliconFlow import SiliconFlow
-from models.deepseek.deepseek import DeepSeek
+from models.deepseek.deepseek_think import DeepSeek
 from models.dashscope.bailian import BaiLian
 from models.dashscope.qwqPlus import QwqPlus
 
@@ -25,6 +26,7 @@ class_map_config: Dict[str, Type["ClassInterface"]] = {
     "g": Gemini,
     "i": DeepSeek,
     "k": QwqPlus,
+    "l": DeepSeekV3,
 }
 
 
---
+++ models/deepseek/deepseek_code.py
@@ -0,0 +1,38 @@
+import os
+from common import common
+from openai import OpenAI
+from class_interface import ClassInterface
+
+client = OpenAI(
+    base_url='https://api.deepseek.com',
+    api_key=os.getenv('DEEP_SEEK_API_KEY')
+)
+
+
+def get_stream(content):
+    return client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "你是迪普，是人工智能助手,用中文详细的回答每一个问题。"},
+            {"role": "user", "content": content},
+        ],
+        stream=True
+    )
+
+
+def req(conclusion=None):
+    try:
+        content = common.get_input(conclusion)
+        stream = get_stream(content)
+        return common.print_stream(stream)
+    except Exception as e:
+        print(f'{type(e).__name__}: {e}')
+
+
+class DeepSeekV3(ClassInterface):
+    def initialize(self):
+        # print(client.models.list())
+        print("正在使用DeepSeek-V3官方大模型")
+
+    def request(self, conclusion=None):
+       return req(conclusion)
