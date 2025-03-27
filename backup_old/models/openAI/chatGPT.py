8da0143d3b3fa0bda9224b82c4748d18c13d979c
Leo <450019458@qq.com>
2025-03-27 16:51:28 +0800

    chore: 移除过时的模型实现和相关文件
    - 删除了多个不再使用的模型文件，包括 deepseekModel.py、dipuDada.py、doubao256kModel.py 等，以简化项目结构
    - 移除了 class_interface.py、common/__init__.py、common/common.py 和 common/markmap.py 文件，减少冗余代码
    - 此次清理旨在提升代码可维护性和项目整洁性

--- backup_old/models/openAI/chatGPT.py
+++
@@ -1,9 +0,0 @@
-from common import common
-from class_interface import ClassInterface
-
-class OpenAI(ClassInterface):
-    def initialize(self):
-        print("正在使用GPT_4o_MINI大模型")
-
-    def request(self, conclusion=None):
-        return common.call_openai_model("gpt-4o-mini", common.get_input(conclusion))
